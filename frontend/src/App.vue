59f1423ca30f130d111383a61acce33b5fc110fd
koreaaerospaceU <zx811@naver.com>
2020-08-26 01:31:00 +0900

    drawer 컴포넌트 제작

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div id="app">
     <div id="app-wrapper">
+      <drawer />
       <router-view />
     </div>
   </div>
 </template>
 
 <script>
+import Drawer from "@/components/Drawer";
 export default {
   name: "App",
+  components: {
+    Drawer
+  },
 
   mounted() {
     this.click = this.$route.name;
@@ -21,8 +26,10 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+}
+
+#app-wrapper {
+  display: flex;
 }
 </style>
---
+++ frontend/src/components/Drawer.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="drawer">
+    <!-- <v-row
+      class="mx-auto my-5 py-3"
+      :class="{ click: click == 'Login' }"
+      @click="click = 'Login'"
+    >
+      <router-link to="/auth">
+        <img id="drawer-home" class="drawer-img" src="@/assets/menu_home.png" />
+      </router-link>
+    </v-row> -->
+    <v-row
+      class="mx-auto my-5 py-3"
+      :class="{ click: click == 'Calender' }"
+      @click="click = 'Calender'"
+    >
+      <router-link to="/calender">
+        <img
+          id="drawer-history"
+          class="drawer-img"
+          src="@/assets/table_icon.svg"
+        />
+      </router-link>
+    </v-row>
+    <v-row
+      class="mx-auto my-5 py-3"
+      :class="{ click: click == 'Chart' }"
+      @click="click = 'Chart'"
+    >
+      <router-link to="/chart">
+        <img
+          id="drawer-route"
+          class="drawer-img"
+          src="@/assets/chart_icon.svg"
+        />
+      </router-link>
+    </v-row>
+    <v-row
+      class="mx-auto my-5 py-3"
+      :class="{ click: click == 'Job' }"
+      @click="click = 'Job'"
+    >
+      <router-link to="/task">
+        <img id="drawer-plan" class="drawer-img" src="@/assets/task_icon.svg" />
+      </router-link>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      click: ""
+    };
+  },
+  mounted() {
+    this.click = this.$route.name;
+  }
+};
+</script>
+
+<style scoped>
+#drawer {
+  background: peachpuff;
+  width: 6% !important;
+  height: 100vh;
+  margin: 0;
+  color: white;
+  text-align: center;
+}
+
+router-link {
+  text-align: center;
+}
+
+.drawer-img {
+  width: 5vw;
+}
+</style>
